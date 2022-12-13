62da00c82bd033ae81ada3b9ec98e476021e2a48
Artaey <95622783+Artaey@users.noreply.github.com>
2022-12-13 11:49:36 +0100

    index live web fix

--- php/index.php
+++ php/index.php
@@ -1,22 +1,19 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "jftest";
+    ob_start();
+    $servername = "betweenfloorboards.com.mysql";
+    $username = "betweenfloorboards_comwordpress";
+    $password = "B0b47h4n";
+    $dbname = "betweenfloorboards_comwordpress";
     $conn = new mysqli($servername, $username, $password, $dbname);
-
     session_start();
     $_SESSION["from"] = "index";
-?>
-
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/formular.css?<?php echo time(); ?>">
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Titillium+Web:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
@@ -25,25 +22,20 @@
 </head>
 <body>
     <div id="logo"> <img src="assets/logojumpingfun.png" alt="JumpingFuns Logo"></div>
-
 <h1 class="overskrift1">Login</h1>
     <div class="container">
-
       <form class="<?php if($_SERVER["REQUEST_METHOD"] == "POST"){ if($_SESSION["iserror"] == "error"){ echo("error");}} ?>" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-        
       <label for="uname"><b>Brugernavn</b></label> <br>
         <div class="inputicon">
           <i class="fa-solid fa-user"></i>
           <input type="text" placeholder="Indtast brugernavn" name="uname" required>
         </div>
-        
         <label for="psw"><b>Kodeord</b></label>  
         <br>
         <div class="inputicon">
           <i class="fa-solid fa-lock"></i>
           <input type="password" placeholder="Indtast kodeord" name="psw" required>
         </div>
-        
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $name = $_POST['uname'];
@@ -51,35 +43,32 @@
             $sql = "SELECT kode FROM bruger WHERE brugernavn='$name'";
             $result = $conn->query($sql);
             $hashed = "";
-
             while($row = $result->fetch_assoc()) {
               $hashed = $row["kode"];
             }
             if(password_verify($psw, $hashed)){
               $_SESSION["userwho"] = $name;
-              header("Location: aarskort.php");
-              
+              header("Location: https://betweenfloorboards.com/semester3/aarskort.php", true, 301);
+              exit;
             }else{
               echo("<span id='error'>Forkert brugernavn eller kodeord</span>");
               $_SESSION["iserror"] = "error";
             }
         }
         ?>
-     
         <span class="psw"><a href="#">Glemt kodeord?</a></span>
-        
         <label id="husk" class="mindretekst">
           <input type="checkbox" checked="checked" name="remember"> Husk mig
         </label>
-      
         <button type="submit">Login</button>
-
         <p class="mindretekst"> Opret dig her 
         <br>
-        <button type="button" class="button button2"  onclick=" location.href='Opret.php'">Opret</button> </p>
+        <button type="button" class="button button2"  onclick=" location.href='opret.php'">Opret</button> </p>
       </form>
-
     </div>
-    
 </body>
 </html>
+
+<?php
+  ob_flush();
+?>
